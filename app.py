cef0627f4fff1e7586d91b2a10266650b88c2c17
srayuth089 <srayuth089@hotmail.com>
2017-09-25 16:57:01 +0700

    Add files via upload

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ def verify():
     # when the endpoint is registered as a webhook, it must echo back
     # the 'hub.challenge' value it receives in the query arguments
     if request.args.get("hub.mode") == "subscribe" and request.args.get("hub.challenge"):
-        if not request.args.get("hub.verify_token") == os.environ["test-toten"]:
+        if not request.args.get("hub.verify_token") == os.environ["VERIFY_TOKEN"]:
             return "Verification token mismatch", 403
         return request.args["hub.challenge"], 200
 
@@ -58,7 +58,7 @@ def send_message(recipient_id, message_text):
     log("sending message to {recipient}: {text}".format(recipient=recipient_id, text=message_text))
 
     params = {
-        "access_token": os.environ["EAAHaZC4G7vbMBAMEeKdmz4Am8oBsFPa7CNLNKcZALTlImAQBwZCOoPUMLORFnNm3WGcclVJrZBwF3VBrdvOPuUsUyRukSLnQq5TUZCA2STxEJntPvhd8ap0NZCW8WCMc64iZCYnqoZCZArK2YFZABLZA2hD53IGKimhNRFSHvEvBg6KBQZDZD"]
+        "access_token": os.environ["PAGE_ACCESS_TOKEN"]
     }
     headers = {
         "Content-Type": "application/json"
